026086b14dea26945f5cae54906da9af51bcdcb1
shir0tetsuo <shadowsword@protonmail.com>
2021-04-04 15:59:58 -0400

    Base Utility

---
+++ npm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#npm i -g npm
+#rm -r node_modules/
+#rm package-lock.json
+#npm cache clean --force
+#npm prune
+#npm update
+#npm install npm@latest -g
+
+npm init
+
+#npm install --save discord.js
+npm install --save express
+#npm install --save firebase-admin
+#npm install --save mongodb
+npm install --save sqlite3
+npm install --save chalk
+npm install --save sequelize
+npm install --save moment
+npm install --save canvas
+npm install --save socket.io
